ffba0962a88a903248d65ddff3517e553d58cef3
edynak-demo <edynakdemo@gmail.com>
2020-04-28 12:54:28 -0400

    implemented GET current user's profile feature

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,9 +1,28 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
 
-// @route Get api/profile
-// @desc  Test route
+const Profile = require('../../models/Profile');
+const User = require('../../models/User');
+
+// @route GET api/profile/me
+// @desc  GET current users profile
 // @access Public
-router.get('/', (req, res) => res.send('Welcome to the profile route'));
+router.get('/me', auth, async (req, res) => {
+  try {
+    const profile = await Profile.findOne({
+      user: req.user.id,
+    }).populate('user', ['name', 'avatar']);
+
+    if (!profile) {
+      return res.status(400).json({ msg: 'There is no profile for this user' });
+    }
+
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
 
 module.exports = router;
