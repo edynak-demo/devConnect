49f6bcd9ce76360c6d3b2f607c9a1ed423a0b79f
edynak-demo <edynakdemo@gmail.com>
2020-04-28 10:42:17 -0400

    implemented user registration

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,8 +2,6 @@ const express = require('express');
 const router = express.Router();
 const gravatar = require('gravatar');
 const bcrypt = require('bcryptjs');
-const jwt = require('jsonwebtoken');
-const config = require('config');
 const { check, validationResult } = require('express-validator');
 
 const User = require('../../models/User');
@@ -11,6 +9,7 @@ const User = require('../../models/User');
 // @route POST api/users
 // @desc  Register user
 // @access Public
+
 router.post(
   '/',
   [
@@ -27,7 +26,48 @@ router.post(
       return res.status(400).json({ errors: errors.array() });
     }
 
-    res.send('Welcome to the users route');
+    const { name, email, password } = req.body;
+
+    try {
+      // See if user exists
+
+      let user = await User.findOne({ email });
+
+      if (user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'User already exists' }] });
+      }
+
+      // Get users gravatar
+
+      const avatar = gravatar.url(email, {
+        s: '200',
+        r: 'pg',
+        d: 'mm',
+      });
+
+      user = new User({
+        name,
+        email,
+        avatar,
+        password,
+      });
+
+      // Encrypt password
+
+      const salt = await bcrypt.genSalt(10);
+
+      user.password = await bcrypt.hash(password, salt);
+
+      await user.save();
+
+      // Return jsonwebtoken
+      res.send('User registered');
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server error');
+    }
   }
 );
 
